424375d92c8c606d5d9f7bc9ed1335bf9c2f425f
Gxb001 <80482371+Gxb001@users.noreply.github.com>
2023-02-22 21:48:25 +0100

    Add files via upload

---
+++ 1-FusionDeTable.py
@@ -0,0 +1,10 @@
+﻿import csv
+
+with open ('Contact2.csv') as Contact2 :
+    data = list(csv.reader(Contact2))
+
+with open ('contact.csv') as contact :
+    data1 = list(csv.reader(contact))
+
+data3 = data1 + data
+print(data3)
